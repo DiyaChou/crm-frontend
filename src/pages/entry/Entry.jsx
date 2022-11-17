cb4bb36e54aff880b79b4e10ac6f8c6f768cbfc9
Diya <91566136+DiyaChou@users.noreply.github.com>
2022-11-17 19:03:36 +0530

    login page

---
+++ src/pages/entry/Entry.jsx
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+import "./Entry.style.css";
+
+const Entry = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [frmLoad, setFrmLoad] = useState("login");
+
+  const handleSubmit = () => {
+    if (!email || !password) alert("fill the form completely");
+  };
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+
+    switch (name) {
+      case "email":
+        setEmail(value);
+        break;
+      case "password":
+        setPassword(value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <div className="entry">
+      {frmLoad === "login" && (
+        <div className="entry__container">
+          <h1 className="entry__title">Client Login</h1>
+          <hr />
+          <form className="entry__form">
+            <div className="entry__form__container">
+              <label className="form__label" htmlFor="client_email">
+                Email
+              </label>
+              <input
+                className="form__input"
+                type="email"
+                placeholder="Enter Email"
+                id="client_email"
+                value={email}
+                name="email"
+                onChange={handleOnChange}
+              />
+            </div>
+            <div className="entry__form__container">
+              <label className="form__label" htmlFor="client_password">
+                Password
+              </label>
+              <input
+                className="form__input"
+                type="password"
+                placeholder="Password"
+                id="client_password"
+                value={password}
+                name="password"
+                onChange={handleOnChange}
+              />
+            </div>
+            <button className="form__submit" onClick={handleSubmit}>
+              Login
+            </button>
+          </form>
+          <hr />
+          <div>
+            <a href="#" onClick={() => setFrmLoad("reset")}>
+              Forgot password?
+            </a>
+          </div>
+        </div>
+      )}
+
+      {frmLoad === "reset" && (
+        <div className="entry__container">
+          <h1 className="entry__title">Reset Password</h1>
+          <hr />
+          <form className="entry__form">
+            <div className="entry__form__container">
+              <label className="form__label" htmlFor="client_email">
+                Email
+              </label>
+              <input
+                className="form__input"
+                type="email"
+                placeholder="Enter Email"
+                id="client_email"
+                value={email}
+                name="email"
+                onChange={handleOnChange}
+              />
+            </div>
+            <button className="form__submit">submit</button>
+          </form>
+          <hr />
+          <div>
+            <a href="#" onClick={() => setFrmLoad("login")}>
+              Go back
+            </a>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Entry;
